c3f19d08703560d273a9af6ba312d371aee476c7
mi-TSUKASA <mi.tsukasa0898@gmail.com>
2021-10-24 18:48:05 +0900

    ポートフォリオ

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const twitter = document.getElementById("twitter");
 const college = document.getElementById("college");
 const goodAt = document.getElementById("goodAt");
 const hobby = document.getElementById("hobby");
+const good = document.getElementById("good");
+const goodText = document.getElementById("good-text");
 
 twitter.onclick = function () {
   location.href = "https://twitter.com/TSUKASA_MIYAKE";
@@ -18,4 +20,8 @@ goodAt.onclick = function () {
 
 hobby.onclick = function () {
   location.href = "https://www.youtube.com/channel/UCZZ0UGjWsRdM8_5bsqtxYaQ";
+};
+
+good.onclick = function () {
+  goodText.textContent = "👍";
 };
